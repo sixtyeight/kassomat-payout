cb974baa07b0627188656f0f244a54f52be10941
sixtyeight <christian_sl@gmx.at>
2016-04-10 02:20:59 +0200

    added floating/floated events for the hopper

--- payoutd.c
+++ payoutd.c
@@ -796,6 +796,24 @@ void mc_handle_events_hopper(struct m_device *device,
 					"PUBLISH hopper-event {\"event\":\"dispensed\",\"amount\":%ld}",
 					poll->events[i].data1);
 			break;
+		case SSP_POLL_FLOATING:
+			asprintf(&response,
+					"{\"event\":\"floating\",\"amount\":%ld,\"cc\":\"%s\"}",
+					poll->events[i].data1, poll->events[i].cc);
+			redisAsyncCommand(db, NULL, NULL, "PUBLISH hopper-event %s",
+					response);
+			free(response);
+			break;
+		case SSP_POLL_FLOATED:
+			asprintf(&response,
+					"{\"event\":\"floated\",\"amount\":%ld,\"cc\":\"%s\"}",
+					poll->events[i].data1, poll->events[i].cc);
+			redisAsyncCommand(db, NULL, NULL, "PUBLISH hopper-event %s",
+					response);
+			free(response);
+			break;
+
+
 		case SSP_POLL_CASHBOX_PAID:
 			asprintf(&response,
 					"{\"event\":\"cashbox paid\",\"amount\":%ld,\"cc\":\"%s\"}",
